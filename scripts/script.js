7a60f6366df710b44e102247908693b00fee7d1f
Mikhail Kopchenov <michaelkopchenov@yandex.ru>
2023-03-08 15:53:45 +0300

    fix: повесить отправку на форму

--- scripts/script.js
+++ scripts/script.js
@@ -16,14 +16,18 @@ function closeClick() {
 closedProfile.addEventListener('click', closeClick);
 
 /*Редактирование профиля*/
-let formElement = document.querySelector('.button__save');
+let formElement = document.querySelector('.popup__form');
 
 function handleFormSubmit (event) {
+    console.log(event.target);
+    console.log(event.currentTarget);
+    console.log(event.type);
     event.preventDefault();
     const name = document.getElementById('name').value;
     document.getElementById('infoname').textContent = name;
     const hobbie = document.getElementById('job').value;
     document.getElementById('infojob').textContent = hobbie;  
+    console.log(event.currentTarget);
     closeClick();
 }
 
